f0a5e2e7b3f1142a012a2602d34226fefe55428a
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2023-01-04 21:57:08 +0300

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -21,7 +21,7 @@ let weather = {
     document.querySelector(".humidity").innerText="Humidity: "+humidity+"%";  
     document.querySelector(".wind").innerText="Wind Speed: "+speed+" km/h";     
     document.querySelector(".weather").classList.remove("loading"); 
-    document.body.style.backgroundImage="url(https://source.unsplash.com/random/200x300/?nature," + name +")";
+    document.body.style.backgroundImage="url(https://source.unsplash.com/random/1600x900/?nature," + name +")";
   },  
   search: function () {  
     this.fetchWeather(document.querySelector(".search-bar").value);  
