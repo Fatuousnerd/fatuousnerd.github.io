6571cd1e780efa9d1a96e5c279bbe76b2f50cd3c
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2023-01-08 14:13:34 +0300

    update

--- Weather.js
+++ Weather.js
@@ -13,15 +13,13 @@ let weather = {
     const {name}=data;  
     const {icon,description}=data.weather[0];  
     const {temp,humidity}=data.main;  
-    const {speed}=data.wind;  
-    //const {pressure}=data.pressure.value;  
+    const {speed}=data.wind; 
     // console.log(name,description,icon,temp,humidity,speed);  
     document.querySelector(".city").innerText="Weather in " + name;  
     document.querySelector(".description").innerText=description;  
     document.querySelector(".temp").innerText=temp + "°C";  
     document.querySelector(".humidity").innerText="Humidity: "+humidity+"%";  
-    document.querySelector(".wind").innerText="Wind Speed: "+speed+" km/h";     
-    //document.querySelector(".pressure").innerText="Pressure: "+pressure+ "°C";     
+    document.querySelector(".wind").innerText="Wind Speed: "+speed+" km/h";   
     document.querySelector(".weather").classList.remove("loading"); 
     document.body.style.backgroundImage="url('https://source.unsplash.com/random/1600x900/?"+ name +")";
   },  
